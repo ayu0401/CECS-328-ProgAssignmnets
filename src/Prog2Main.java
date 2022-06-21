5b1f7266b8a2e4c179df397006e250ce8da727dc
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2022-06-20 23:09:25 -0700

    main function elements added

--- src/Prog2Main.java
+++ src/Prog2Main.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Prog2Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-        int[] array;
+        boolean exit = true;
+        int userIn;
+        System.out.println("------Heap Creation Program------");
 
+        while(exit){
+            System.out.println("--Program Options--\n1)Create Heap\n2)Input values in CSV format into heap\n" +
+                    "3)Insert one element into heap\n4)Pop and element out of heap\n5)Display Sorted Heap\n" +
+                    "6)Provide a length of n to generate random heap\n7)Quit\n");
 
+            System.out.println("Enter number for the function you want to use!: ");
+
+            Scanner sc = new Scanner(System.in);
+            userIn = sc.nextInt();
+
+            switch(userIn){
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    System.out.println("Exiting Program! Thanks for using Heap Creation!");
+                    exit = false;
+            }
+        }
 
     }
 }
