4813edad91c2464cc0fb8bbb2a41e7d7830bbdf6
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2022-06-22 18:17:56 -0700

    heap java updtae, main update

--- src/Heap.java
+++ src/Heap.java
@@ -1,46 +1,51 @@
-//import java.util.Arrays;
-//public class Heap {
-//
-//    public int[] array;
-//    public int heapSize;
-//
-//    //basic constructor to build the Heap
-//    public Heap(int arraySize){
-//        array = new int[arraySize];
-//        heapSize = arraySize;
-//    }
-//
-//
-//
-//
-//
-////Heap Functions
+import java.util.Arrays;
+public class Heap {
+
+    private int[] array;
+    private int heapSize;
+
+    //basic constructor to build the Heap
+    public Heap(int arraySize) {
+        array = new int[arraySize];
+        heapSize = arraySize;
+    }
+
+
+
+
+
+//Heap Functions
 //    public boolean isFull(){
 //
 //    }
-//
-//    public static insert(int[] A, int num){
-//
-//    }
-//
-//    public static pop(int[] A, )
-//
-//
-//
-//
-////Prints Array into String
-//    public String printArray(int[] A){
-//        return Arrays.toString(A);
-//    }
-//
-//
-//
-//
-////getter method
-//
-//
-//    public int[] getArray() {
-//        return array;
-//    }
-//}
-//
+
+    public static heapify()
+
+    public static insert(int[] A, int num){
+
+    }
+
+    public static pop(int[] A, )
+
+
+
+
+//Prints Array into String
+    public String printArray(int[] A){
+        return Arrays.toString(A);
+    }
+
+
+
+
+//getter method
+
+
+    public int[] getArray() {
+        return array;
+    }
+    public int getHeapSize(){
+        return heapSize;
+    }
+}
+
--- src/Prog2Main.java
+++ src/Prog2Main.java
@@ -1,12 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Prog2Main {
     public static void main(String[] args) {
         boolean exit = true;
         int userIn;
+        Heap hp = null;
+
         System.out.println("------Heap Creation Program------");
 
-        while(exit){
+
+
+        do {
             System.out.println("--Program Options--\n1)Create Heap\n2)Input values in CSV format into heap\n" +
                     "3)Insert one element into heap\n4)Pop and element out of heap\n5)Display Sorted Heap\n" +
                     "6)Provide a length of n to generate random heap\n7)Quit\n");
@@ -16,9 +21,16 @@ public class Prog2Main {
             Scanner sc = new Scanner(System.in);
             userIn = sc.nextInt();
 
-            switch(userIn){
+            switch (userIn) {
                 case 1:
+                    System.out.println("Enter a number to set length of Heap: ");
+                    int userNum = sc.nextInt();
+                    hp = new Heap(userNum);
+                    break;
                 case 2:
+                    assert hp != null;
+                    System.out.println(Arrays.toString(hp.getArray()));
+                    break;
                 case 3:
                 case 4:
                 case 5:
@@ -27,7 +39,7 @@ public class Prog2Main {
                     System.out.println("Exiting Program! Thanks for using Heap Creation!");
                     exit = false;
             }
-        }
+        }while(exit);
 
     }
 }
