a506bd5134abe485e57d40403b6c29116b54447d
Annihilator3180 <munozjacob48@yahoo.com>
2022-06-24 15:27:52 -0700

    Update Heap.java

--- src/Heap.java
+++ src/Heap.java
@@ -19,14 +19,30 @@ public class Heap {
 //
 //    }
 
-    public static heapify()
+    public static heapify(int[] array, int i){
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if(left < heapSize[array] && array[left] > array[largest])
+            largest = left;
+        else
+            largest = i;
+        else if (right < )
+
+    }
 
     public static insert(int[] A, int num){
 
     }
 
-    public static pop(int[] A, ){
-        
+    public static pop(int[] A, int num ){
+        int lastElement = A[num - 1];
+        A[0] = lastElement;
+        num -= 1;
+        heapify(A, num, 0);
+        return num;
+
     }
 
 
