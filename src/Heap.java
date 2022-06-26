3c1ada0dc2d93f607267ec7641242f23874ebc38
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2022-06-25 21:12:52 -0700

    heap and main update

--- src/Heap.java
+++ src/Heap.java
@@ -1,10 +1,15 @@
-import java.util.*;
+import java.util.Arrays;
 public class Heap {
 
     private int[] array;
     private int heapSize;
 
+
+
     //basic constructor to build the Heap
+    public Heap(){
+        array = null;
+    }
     public Heap(int arraySize) {
         array = new int[arraySize];
         heapSize = arraySize;
@@ -31,18 +36,53 @@ public class Heap {
 
 
 //heap functions
-    public static void heapify(int[] A, int i){
+
+    public static void swap(Heap hp, int i, int j){
+        int temp;
+        temp = hp.getArray()[i];
+        hp.getArray()[i] = hp.getArray()[j];
+        hp.getArray()[j] = temp;
+    }
+
+
+    public static void heapify(Heap hp, int i){
         int l = Left(i);
         int r = Right(i);
+        int smallest;
 
-        if (l <= )
+        if (l >=  hp.heapSize && hp.getArray()[l] < hp.getArray()[l]){
+             smallest = l;
+        }
+        else{
+            smallest = i;
+        }
+        if (r >= hp.heapSize && hp.getArray()[r] < hp.getArray()[r]){
+            smallest = r;
+        }
+        if (smallest != i){
+            swap(hp, i, smallest);
+            heapify(hp, smallest);
+        }
     }
 
-    public static void insert(int[] A, int num){
+    public static void BuildHeap(Heap hp){
+        for (int i = hp.getHeapSize()/2; i>= 1; i--){
+            heapify(hp, i);
+        }
+    }
+
+    public void insert(int num){
+
 
     }
 
-    public static pop(int[] A, )
+    public void pop(Heap hp){
+        int lastElement = hp.getArray()[hp.getHeapSize() - 1];
+        hp.getArray()[0] = lastElement;
+        int num = getHeapSize() - 1;
+        decHeapSize();
+        heapify(hp,num);
+    }
 
 
 
@@ -64,5 +104,21 @@ public class Heap {
     public int getHeapSize(){
         return heapSize;
     }
+
+
+
+    //setter method
+    public void incHeapSize() {
+        heapSize++;
+    }
+    public void decHeapSize() {
+        heapSize--;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
 }
+
+
 
--- src/Prog2Main.java
+++ src/Prog2Main.java
@@ -6,13 +6,13 @@ public class Prog2Main {
         boolean exit = true;
         int userIn;
         Heap hp = null;
-
         System.out.println("------Heap Creation Program------");
 
 
 
         do {
-            System.out.println("--Program Options--\n1)Create Heap\n2)Input values in CSV format into heap\n" +
+            System.out.println("--Program Options--\n1)Create Heap\n2)Create Heap by inputting values " +
+                    "in CSV format into heap\n" +
                     "3)Insert one element into heap\n4)Pop and element out of heap\n5)Display Sorted Heap\n" +
                     "6)Provide a length of n to generate random heap\n7)Quit\n");
 
@@ -21,6 +21,7 @@ public class Prog2Main {
             Scanner sc = new Scanner(System.in);
             userIn = sc.nextInt();
 
+
             switch (userIn) {
                 case 1:
                     System.out.println("Enter a number to set length of Heap: ");
@@ -28,11 +29,29 @@ public class Prog2Main {
                     hp = new Heap(userNum);
                     break;
                 case 2:
-                    assert hp != null;
+                    sc.nextLine();
+                    System.out.println("Enter a comma separated list of integer numbers to enter into Heap: ");
+                    String userString = sc.nextLine();
+                    String[] temp = userString.split(",");
+                    hp = new Heap(temp.length);
+                    for(int i = 0; i < temp.length; i++){
+                        hp.getArray()[i] = Integer.parseInt(temp[i]);
+                    }
+                    Heap.BuildHeap(hp);
                     System.out.println(Arrays.toString(hp.getArray()));
                     break;
                 case 3:
+                    System.out.println("Enter one integer element you want to insert into Heap: ");
+                    userNum = sc.nextInt();
+                    assert hp != null;
+                    break;
+
                 case 4:
+                    System.out.println("----Now root element from heap!----");
+                    assert hp != null;
+                    hp.pop(hp);
+                    System.out.println(Arrays.toString(hp.getArray()));
+                    break;
                 case 5:
                 case 6:
                 case 7:
